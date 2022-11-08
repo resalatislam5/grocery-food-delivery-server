09f07fa52b047e47d53241caa3e77df886794592
Resalat <resalatislam5@gmail.com>
2022-11-08 17:01:56 +0600

    home products

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@grover-grocery.gqn8qjj.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run(){
+    try{
+        const productsCollection = client.db('grocery').collection('products')
+        app.get('/homeproducts', async(req,res)=>{
+            const query = {}
+            const cursor = productsCollection.find(query);
+            const products = await cursor.limit(3).toArray();
+            console.log(products)
+            res.send(products)
+        })
+
+    }
+    finally{
+
+    }
+}
+run().catch(console.dir)
 app.get('/',(req,res)=>{
     res.send('Grocery Server Running....')
 })
